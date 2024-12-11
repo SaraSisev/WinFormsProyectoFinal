30b8a9437084d4fd05f58fe4244a72602b0597fb
Eliseo Villalobos <elibat1000@gmail.com>
2024-12-11 14:49:12 -0600

    quite messi

--- WinFormsProyectoFinal/obj/Debug/net6.0-windows/WinFormsProyectoFinal.AssemblyInfo.cs
+++ WinFormsProyectoFinal/obj/Debug/net6.0-windows/WinFormsProyectoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c901cfac38abb6d58c106699a404737fd36a1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f3d412388444756b4ee70d237b92c3e104c4a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
