e2680030799331a08291782341d85a74e048b530
Eliseo Villalobos <elibat1000@gmail.com>
2024-12-11 14:39:31 -0600

    cambios

--- WinFormsProyectoFinal/Form3.Designer.cs
+++ WinFormsProyectoFinal/Form3.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             buttonlogout = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // buttonlogout
@@ -41,19 +44,53 @@
             buttonlogout.UseVisualStyleBackColor = true;
             buttonlogout.Click += buttonlogout_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(163, 179);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(465, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(323, 287);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 3;
+            label3.Text = "label3";
+            // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(buttonlogout);
             Name = "Form3";
             Text = "Form3";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button buttonlogout;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- WinFormsProyectoFinal/obj/Debug/net6.0-windows/WinFormsProyectoFinal.AssemblyInfo.cs
+++ WinFormsProyectoFinal/obj/Debug/net6.0-windows/WinFormsProyectoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+303e570ffe4192440b592cffc6637ffcb2534c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
