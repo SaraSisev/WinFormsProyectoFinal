d224b2ee7e90240e16b23b0de6e2c24fc80e924b
Sara Cuevas <saraalexandrac@hotmail.com>
2024-12-12 18:09:11 -0600

    Guardando cambios

--- WinFormsProyectoFinal/FormAltas.Designer.cs
+++ WinFormsProyectoFinal/FormAltas.Designer.cs
@@ -28,79 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAltas));
             buttonLogoutA = new Button();
-            textBoxAlID = new TextBox();
             textBoxAlPre = new TextBox();
-            textBoxAlExi = new TextBox();
             richTextBoxAlDes = new RichTextBox();
             buttonAgregar = new Button();
+            btnRegresar = new PictureBox();
+            pictureBox2 = new PictureBox();
+            btnImagen = new Button();
+            numId = new NumericUpDown();
+            numExistencias = new NumericUpDown();
+            numVentas = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)btnRegresar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numId).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numExistencias).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numVentas).BeginInit();
             SuspendLayout();
             // 
             // buttonLogoutA
             // 
-            buttonLogoutA.Location = new Point(628, 374);
+            buttonLogoutA.Location = new Point(471, 11);
+            buttonLogoutA.Margin = new Padding(2);
             buttonLogoutA.Name = "buttonLogoutA";
-            buttonLogoutA.Size = new Size(112, 34);
+            buttonLogoutA.Size = new Size(78, 20);
             buttonLogoutA.TabIndex = 0;
             buttonLogoutA.Text = "LogOut";
             buttonLogoutA.UseVisualStyleBackColor = true;
             buttonLogoutA.Click += buttonLogoutA_Click;
             // 
-            // textBoxAlID
-            // 
-            textBoxAlID.Location = new Point(68, 78);
-            textBoxAlID.Name = "textBoxAlID";
-            textBoxAlID.PlaceholderText = "ID";
-            textBoxAlID.Size = new Size(146, 31);
-            textBoxAlID.TabIndex = 1;
-            // 
             // textBoxAlPre
             // 
-            textBoxAlPre.Location = new Point(68, 284);
+            textBoxAlPre.Location = new Point(48, 170);
+            textBoxAlPre.Margin = new Padding(2);
             textBoxAlPre.Name = "textBoxAlPre";
             textBoxAlPre.PlaceholderText = "Precio";
-            textBoxAlPre.Size = new Size(146, 31);
+            textBoxAlPre.Size = new Size(103, 23);
             textBoxAlPre.TabIndex = 3;
             textBoxAlPre.TextChanged += textBox3_TextChanged;
             // 
-            // textBoxAlExi
-            // 
-            textBoxAlExi.Location = new Point(68, 340);
-            textBoxAlExi.Name = "textBoxAlExi";
-            textBoxAlExi.PlaceholderText = "Existencia";
-            textBoxAlExi.Size = new Size(146, 31);
-            textBoxAlExi.TabIndex = 4;
-            // 
             // richTextBoxAlDes
             // 
-            richTextBoxAlDes.Location = new Point(68, 115);
+            richTextBoxAlDes.Location = new Point(48, 69);
+            richTextBoxAlDes.Margin = new Padding(2);
             richTextBoxAlDes.Name = "richTextBoxAlDes";
-            richTextBoxAlDes.Size = new Size(150, 144);
+            richTextBoxAlDes.Size = new Size(106, 88);
             richTextBoxAlDes.TabIndex = 5;
             richTextBoxAlDes.Text = "";
             // 
             // buttonAgregar
             // 
-            buttonAgregar.Location = new Point(79, 395);
+            buttonAgregar.Location = new Point(11, 327);
+            buttonAgregar.Margin = new Padding(2);
             buttonAgregar.Name = "buttonAgregar";
-            buttonAgregar.Size = new Size(112, 34);
+            buttonAgregar.Size = new Size(78, 20);
             buttonAgregar.TabIndex = 6;
             buttonAgregar.Text = "Agregar";
             buttonAgregar.UseVisualStyleBackColor = true;
+            buttonAgregar.Click += buttonAgregar_Click_1;
+            // 
+            // btnRegresar
+            // 
+            btnRegresar.Image = (Image)resources.GetObject("btnRegresar.Image");
+            btnRegresar.Location = new Point(503, 305);
+            btnRegresar.Name = "btnRegresar";
+            btnRegresar.Size = new Size(45, 42);
+            btnRegresar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnRegresar.TabIndex = 7;
+            btnRegresar.TabStop = false;
+            btnRegresar.Click += btnRegresar_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(261, 54);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(138, 139);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // btnImagen
+            // 
+            btnImagen.Location = new Point(295, 199);
+            btnImagen.Name = "btnImagen";
+            btnImagen.Size = new Size(104, 23);
+            btnImagen.TabIndex = 9;
+            btnImagen.Text = "Agregar Imagen";
+            btnImagen.UseVisualStyleBackColor = true;
+            btnImagen.Click += button1_Click;
+            // 
+            // numId
+            // 
+            numId.Location = new Point(48, 41);
+            numId.Name = "numId";
+            numId.Size = new Size(120, 23);
+            numId.TabIndex = 10;
+            // 
+            // numExistencias
+            // 
+            numExistencias.Location = new Point(48, 231);
+            numExistencias.Name = "numExistencias";
+            numExistencias.Size = new Size(120, 23);
+            numExistencias.TabIndex = 11;
+            // 
+            // numVentas
+            // 
+            numVentas.Location = new Point(48, 285);
+            numVentas.Name = "numVentas";
+            numVentas.Size = new Size(120, 23);
+            numVentas.TabIndex = 12;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 213);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 13;
+            label1.Text = "Existencias";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(51, 267);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 14;
+            label2.Text = "Ventas";
             // 
             // FormAltas
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(560, 358);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(numVentas);
+            Controls.Add(numExistencias);
+            Controls.Add(numId);
+            Controls.Add(btnImagen);
+            Controls.Add(pictureBox2);
+            Controls.Add(btnRegresar);
             Controls.Add(buttonAgregar);
             Controls.Add(richTextBoxAlDes);
-            Controls.Add(textBoxAlExi);
             Controls.Add(textBoxAlPre);
-            Controls.Add(textBoxAlID);
             Controls.Add(buttonLogoutA);
+            Margin = new Padding(2);
             Name = "FormAltas";
             Text = "FormAltas";
+            ((System.ComponentModel.ISupportInitialize)btnRegresar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numId).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numExistencias).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numVentas).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -108,10 +190,16 @@
         #endregion
 
         private Button buttonLogoutA;
-        private TextBox textBoxAlID;
         private TextBox textBoxAlPre;
-        private TextBox textBoxAlExi;
         private RichTextBox richTextBoxAlDes;
         private Button buttonAgregar;
+        private PictureBox btnRegresar;
+        private PictureBox pictureBox2;
+        private Button btnImagen;
+        private NumericUpDown numId;
+        private NumericUpDown numExistencias;
+        private NumericUpDown numVentas;
+        private Label label1;
+        private Label label2;
     }
 }
--- WinFormsProyectoFinal/FormAltas.cs
+++ WinFormsProyectoFinal/FormAltas.cs
@@ -27,16 +27,49 @@ namespace WinFormsProyectoFinal
 
         private void buttonAgregar_Click(object sender, EventArgs e)
         {
+            
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Choose Image(*.jpg; *.png; *.gif)|*.jpg; *.png; *.gif";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image = Image.FromFile(opf.FileName);
+            }
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            Form4 inicio = new Form4();
+            inicio.Show();
+            this.Close();
+        }
+
+        private void buttonAgregar_Click_1(object sender, EventArgs e)
+        {
             try
             {
                 Conexion conn = new Conexion();//variable que nos permite establecer la conexion con la base de datos
                 conn.conexionBD();//llamamos al metodo que conecta a la base de datos
-                MySqlCommand cmd1 = new MySqlCommand("insert into productos (id,imagen,descripcion,precio,existencias) values (@id, @imagen, @descripcion, @precio, @existencias)", conn.conexionBD());
-                cmd1.Parameters.AddWithValue("@id", textBoxAlID);
-                //cmd1.Parameters.AddWithValue("@imagen", );
+                MemoryStream ms = new MemoryStream();
+                pictureBox2.Image.Save(ms, pictureBox2.Image.RawFormat);
+                byte[] img = ms.ToArray();
+
+                MySqlCommand cmd1 = new MySqlCommand("insert into productos (id,imagen,descripcion,precio,existencias,ventas) values (@id, @imagen, @descripcion, @precio, @existencias, @ventas)", conn.conexionBD());
+                cmd1.Parameters.AddWithValue("@id", numId.Value);
+                cmd1.Parameters.AddWithValue("@imagen", img);
                 cmd1.Parameters.AddWithValue("@descripcion", richTextBoxAlDes.Text);
                 cmd1.Parameters.AddWithValue("@precio", textBoxAlPre.Text);
-                cmd1.Parameters.AddWithValue("@existencias", textBoxAlExi.Text);
+                cmd1.Parameters.AddWithValue("@existencias", numExistencias.Value);
+                cmd1.Parameters.AddWithValue("@ventas", numVentas.Value);
                 cmd1.ExecuteNonQuery();//comando que da entender que no devuelve resultados, osea un conjunto de datos
                 MessageBox.Show("Se han agregado los datos");
             }
@@ -44,12 +77,6 @@ namespace WinFormsProyectoFinal
             {
                 MessageBox.Show(ex.ToString());
             }
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
